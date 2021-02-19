4bb693f63850dc5ee9b59b816fb4cd9379460473
AnDx25 <a.dixit@iiitmanipur.ac.in>
2021-02-19 23:06:00 +0530

    Completed Navigation Functionality

--- src/Controller.js
+++ src/Controller.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './screens/home/Home'
+import Recuirtment from './screens/recuirtment/Recuirtment'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import DashboardLayout from './components/dashboardLayout/dashboard'
 function Controlller() {
@@ -8,7 +10,8 @@ function Controlller() {
       <Router>
         <DashboardLayout>
         <Switch>
-          <Route/>
+          <Route exact path="/" render={(props)=>{<Home {...props}/>}}/>
+          <Route path="/recuirtment" render={(props)=>{<Recuirtment {...props}/>}}/>
         </Switch>
         </DashboardLayout>
       </Router>
--- src/components/dashboardLayout/dashboard.js
+++ src/components/dashboardLayout/dashboard.js
@@ -1,10 +1,58 @@
 import React from 'react'
-
+import {VscHome} from 'react-icons/vsc'
+import {MdPeopleOutline} from 'react-icons/md'
+import {CgFileDocument} from 'react-icons/cg'
+import {VscGraph} from 'react-icons/vsc'
+import {FiSettings} from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+const navLinks=[
+    {
+        name:"Dashboard",
+        icon:<VscHome/>,
+        link:"/"
+    },
+    {
+        name:"Recuirtment",
+        icon:<MdPeopleOutline/>,
+        link:"/recuirtment"
+    },
+    {
+        name:"Onboarding",
+        icon:<CgFileDocument/>,
+        link:"/onboarding"
+    },
+    {
+        name:"Reports",
+        icon:<VscGraph/>,
+        link:"/reports"
+    },
+    {
+        name:"Settings",
+        icon:<FiSettings/>,
+        link:"/settings"
+    },
+]
 const DashboardLayout=({children})=>{
+    console.log(children)
     return(
         <div className="dashboard-layout">
              <nav className="side-nav-bar">
-                this is nav
+                <div className="logo-container">
+                    <h2>Newton HR</h2>
+                </div>
+                <div className="nav-container"> 
+                    {
+                        navLinks.map((navItem,index)=>(
+                            <Link to={navItem.link}>
+                            <div key={index} className="nav-item">
+                                {navItem.icon}
+                               <label className="item-name"> {navItem.name}</label>
+                            </div>
+                            </Link>
+
+                        ))
+                    }
+                </div>
             </nav>
             <div className="main-container">
                 here are children
